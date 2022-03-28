fdddc32ea58533bb1868266d80f78681a3299647
Zhur.a.vleva <Zhur.a.vleva.Darya21@gmail.com>
2022-03-28 23:15:27 +0300

    [ADDED]: menu items, styles, colors

--- app/src/main/java/com/example/telegramclone/MainActivity.kt
+++ app/src/main/java/com/example/telegramclone/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.telegramclone
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.example.telegramclone.databinding.ActivityMainBinding
@@ -8,8 +10,10 @@ import com.mikepenz.materialdrawer.AccountHeader
 import com.mikepenz.materialdrawer.AccountHeaderBuilder
 import com.mikepenz.materialdrawer.Drawer
 import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.DividerDrawerItem
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
 import com.mikepenz.materialdrawer.model.ProfileDrawerItem
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
 
 class MainActivity : AppCompatActivity() {
 
@@ -59,13 +63,74 @@ class MainActivity : AppCompatActivity() {
             .withActionBarDrawerToggle(true)
             .withSelectedItem(-1)
             .withAccountHeader(header)
-            .addDrawerItems( // enumerate menu's items
+            .addDrawerItems(
+                // enumerate menu items
                 PrimaryDrawerItem()
                     .withIdentifier(100)
                     .withIconTintingEnabled(true)
                     .withName(R.string.menu_create_group)
                     .withSelectable(false)
-            ).build()
+                    .withIcon(R.drawable.ic_menu_create_group),
+                PrimaryDrawerItem()
+                    .withIdentifier(101)
+                    .withIconTintingEnabled(true)
+                    .withName(R.string.menu_create_secret_chat)
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_create_secret_chat),
+                PrimaryDrawerItem()
+                    .withIdentifier(102)
+                    .withIconTintingEnabled(true)
+                    .withName(R.string.menu_create_channel)
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_create_channel),
+                PrimaryDrawerItem()
+                    .withIdentifier(103)
+                    .withIconTintingEnabled(true)
+                    .withName(R.string.menu_contacts)
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_contacts),
+                PrimaryDrawerItem()
+                    .withIdentifier(104)
+                    .withIconTintingEnabled(true)
+                    .withName(R.string.menu_calls)
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_calls),
+                PrimaryDrawerItem()
+                    .withIdentifier(105)
+                    .withIconTintingEnabled(true)
+                    .withName(R.string.menu_favorites)
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_favorites),
+                PrimaryDrawerItem()
+                    .withIdentifier(106)
+                    .withIconTintingEnabled(true)
+                    .withName(R.string.menu_settings)
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_settings),
+                DividerDrawerItem(),
+                PrimaryDrawerItem()
+                    .withIdentifier(107)
+                    .withIconTintingEnabled(true)
+                    .withName(R.string.menu_invite_friends)
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_invite_friends),
+                PrimaryDrawerItem()
+                    .withIdentifier(108)
+                    .withIconTintingEnabled(true)
+                    .withName(R.string.menu_questions)
+                    .withSelectable(false)
+                    .withIcon(R.drawable.ic_menu_questions),
+            ).withOnDrawerItemClickListener(object : Drawer.OnDrawerItemClickListener {
+                override fun onItemClick(
+                    view: View?,
+                    position: Int,
+                    drawerItem: IDrawerItem<*>
+                ): Boolean {
+                    Toast.makeText(applicationContext, position.toString(), Toast.LENGTH_SHORT).show()
+                    return false
+                }
+            })
+            .build()
     }
 
 }
